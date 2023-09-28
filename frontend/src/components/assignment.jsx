436413808cdc6db3cab00a4d66af95d0bb55ecd0
Hamad-Younas <140589341+Hamad-Younas@users.noreply.github.com>
2023-09-28 13:10:59 +0500

    upload assignments and quizes student UI

--- frontend/src/components/assignment.jsx
+++ frontend/src/components/assignment.jsx
@@ -1,15 +1,110 @@
 import Footer from "./layout/footer";
 import Navbar from "./layout/navbar";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-const Modal = ({ showModal, setShowModal }) => {
-    // You can customize the modal content here
-    //   
+const Modal = ({ data, showModal, setShowModal, updateSubmitAtIndex, index }) => {
+    const [files, setFile] = useState(null);
+    const [inputs, setInputs] = useState([{ question: '', number: 0 }]);
+    const [selectedFileName, setSelectedFileName] = useState('');
+    useEffect(() => {
+        console.log(data)
+    }, []); // State to display selected file name
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        const newFile = event.dataTransfer.files[0]; // Grab only the first dropped file
+        setFile(newFile);
+        setSelectedFileName(newFile.name);
+    };
+
+    const handleDragOver = (event) => {
+        event.preventDefault();
+    };
+
+    const handleDragEnter = (event) => {
+        event.preventDefault();
+    };
+
+    const handleDragLeave = (event) => {
+        event.preventDefault();
+    };
+
+    const handleFileInputChange = (event) => {
+        const newFile = event.target.files[0]; // Grab only the first selected file
+        setFile(newFile);
+        setSelectedFileName(newFile.name);
+    };
+
+    const handleSubmit = async () => {
+
+        const name = data.Name
+        const description = data.Description
+        const date = data.Date
+        const category = data.Category
+        const username = localStorage.getItem("username")
+
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('description', description);
+        formData.append('date', date);
+        formData.append('category', category);
+        formData.append('Marks', data.Marks);
+        formData.append('file', files);
+        formData.append('username', username);
+        formData.append('teacher', data.TeacherName);
+
+
+        inputs.forEach((input, index) => {
+            formData.append(`inputs[${index}][question]`, input.question);
+            formData.append(`inputs[${index}][number]`, input.number);
+        });
+
+        try {
+            const response = await axios.post('http://localhost:4000/stuassignment/add', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            if (response.data.message === "added") {
+                updateSubmitAtIndex(index, 1)
+            } else if (response.data.message === "already") {
+                document.getElementById("desk-error").innerHTML = "Course ALREADY EXIST";
+                document.getElementById("desk-error").style.color = "red";
+                document.getElementById("desk-error").style.display = "block";
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+
+    const handleUpload = () => {
+        // Handle file upload logic
+        console.log(files) // Reset file name display
+        setShowModal(false);
+        handleSubmit()
+    };
+
+    const handleCancel = () => {
+        setFile(null);
+        setSelectedFileName('');
+        setShowModal(false);
+    };
+
     return (
         <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center ${showModal ? '' : 'hidden'}`}>
             <div class="bg-gray-500 h-screen w-screen sm:px-8 md:px-16 sm:py-8">
                 <main class="container mx-auto max-w-screen-lg h-full">
-                    <article aria-label="File Upload Modal" class="relative h-full flex flex-col bg-white shadow-xl rounded-md" ondrop="dropHandler(event);" ondragover="dragOverHandler(event);" ondragleave="dragLeaveHandler(event);" ondragenter="dragEnterHandler(event);">
+                    <article
+                        aria-label="File Upload Modal"
+                        className="relative h-full flex flex-col bg-white shadow-xl rounded-md"
+                        onDrop={handleDrop}
+                        onDragOver={handleDragOver}
+                        onDragEnter={handleDragEnter}
+                        onDragLeave={handleDragLeave}
+                    >
                         <div id="overlay" class="w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md">
                             <i>
                                 <svg class="fill-current w-12 h-12 mb-3 text-blue-700" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
@@ -24,15 +119,23 @@ const Modal = ({ showModal, setShowModal }) => {
                                 <p class="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
                                     <span>Drag and drop your</span>&nbsp;<span>files anywhere or</span>
                                 </p>
-                                <input id="hidden-input" type="file" multiple class="hidden" />
-                                <button id="button" class="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
+                                <input
+                                    id="hidden-input"
+                                    name="file"
+                                    type="file"
+                                    className="hidden"
+                                    onChange={handleFileInputChange}
+                                />
+                                <button
+                                    id="button"
+                                    className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
+                                    onClick={() => document.getElementById('hidden-input').click()} // Trigger file input click
+                                >
                                     Upload a file
                                 </button>
                             </header>
 
-                            <h1 class="pt-8 pb-3 font-semibold sm:text-lg text-gray-900">
-                                To Upload
-                            </h1>
+                            {selectedFileName && <p>{selectedFileName}</p>} {/* Display selected file name */}
 
                             <ul id="gallery" class="flex flex-1 flex-wrap -m-1">
                                 <li id="empty" class="h-full w-full text-center flex flex-col items-center justify-center items-center">
@@ -43,12 +146,21 @@ const Modal = ({ showModal, setShowModal }) => {
                         </section>
 
                         <footer class="flex justify-end px-8 pb-8 pt-4">
-                            <button id="submit" class="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none" onClick={() => setShowModal(false)}>
+                            <button
+                                id="submit"
+                                className="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none"
+                                onClick={handleUpload}
+                            >
                                 Upload now
                             </button>
-                            <button id="cancel" class="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none" onClick={() => setShowModal(false)}>
+                            <button
+                                id="cancel"
+                                className="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
+                                onClick={handleCancel}
+                            >
                                 Cancel
                             </button>
+
                         </footer>
                     </article>
                 </main>
@@ -113,8 +225,55 @@ const Modal = ({ showModal, setShowModal }) => {
 };
 
 const Assignment = () => {
-    const [files, setFiles] = useState({});
+    const [submit, setsubmit] = useState([]);
+    const [files, setFiles] = useState([]);
+    const [data, setdata] = useState([]);
+    const [udata, setudata] = useState([]);
+    const [index, setindex] = useState([]);
 
+    const [showModal, setShowModal] = useState(false);
+    const username = localStorage.getItem("username")
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+
+    const fetchData = async () => {
+        const Category = "assignment"
+        try {
+            const response = await axios.get(`http://localhost:4000/assignment/getbycategory?Category=${Category}`);
+            if (response.status !== 200) {
+                throw new Error("Request failed.");
+            }
+            const fetchedData = response.data;
+            setdata(fetchedData);
+            setsubmit(new Array(fetchedData.length).fill(0));
+        } catch (error) {
+            const logLevel = 'error'; // Define the log level for errors
+            const errorMessage = error.message; // Get the error message
+            const where = "student assignment"; // Get the error message
+            const data1 = {
+                level: logLevel,
+                message: errorMessage,
+                where: where,
+            }
+            axios.post('http://localhost:4000/logs/add', data1)
+                .then((response) => {
+                    console.log("done");
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+        }
+    };
+    const updateSubmitAtIndex = (index, value) => {
+        setsubmit(prevSubmit => {
+            const updatedSubmit = [...prevSubmit]; // Create a copy of the 'submit' array
+            updatedSubmit[index] = value; // Update the value at the specified index
+            return updatedSubmit; // Set the state with the updated array
+        });
+    };
     const addFile = (file) => {
         const isImage = file.type.match('image.*');
         const objectURL = URL.createObjectURL(file);
@@ -140,44 +299,53 @@ const Assignment = () => {
     const handleClearSelection = () => {
         setFiles({});
     };
-    const [showModal, setShowModal] = useState(false);
+
+    const handleupload = (d, index) => {
+        setShowModal(true)
+        setudata(d)
+        setindex(index)
+    }
+
     return (
         <div className="assign_page">
             <Navbar />
             <div className="content flex flex-col mt-16 mb-8 justify-center items-center sm:mx-[12%]">
-            <h1 className="text-3xl">Assignments</h1>
-                <Modal showModal={showModal} setShowModal={setShowModal} />
-                <div className="max-w-4xl px-10 bg-white rounded-lg shadow-md">
-                    <div className="flex justify-between items-center">
-                        <span className="font-light text-gray-600">mar 10, 2019</span>
-                        <a className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500 cursor-pointer" onClick={() => setShowModal(true)} >
-                            Upload
-                        </a>
-                    </div>
-                    <div className="mt-2">
-                        <a className="text-2xl text-gray-700 font-bold hover:text-gray-600" href="/">
-                            Assignment
-                        </a>
-                        <p className="mt-2 text-gray-600">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque.
-                            Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!
-                        </p>
-                    </div>
-                    <div className="flex justify-between items-center mt-4">
-                        <a className="text-blue-600 hover:underline" href="/">
-                            Download
-                        </a>
-                        <div>
-                            <a className="flex items-center" href="/">
-                                <img
-                                    className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
-                                    src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
-                                    alt="avatar"
-                                />
-                                <h1 className="text-gray-700 font-bo    ld">Khatab wedaa</h1>
-                            </a>
+                <h1 className="text-3xl">Assignments</h1>
+                <Modal data={udata} showModal={showModal} setShowModal={setShowModal} updateSubmitAtIndex={updateSubmitAtIndex} index={index} />
+                <div className="w-full max-w-4xl">
+                    {data && data.map((d, index) => (
+                        <div key={d._id} className="px-10 bg-white rounded-lg shadow-md">
+                            <div className="flex justify-between items-center">
+                                <span className="font-light text-gray-600">{d.Date}</span>
+                                <a className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500 cursor-pointer" onClick={() => handleupload(d, index)} >
+                                    {submit[index] == 1 ? "Submitted" : "Upload"}
+                                </a>
+                            </div>
+                            <div className="mt-2">
+                                <a className="text-2xl text-gray-700 font-bold hover:text-gray-600" href="/">
+                                    {d.Name}
+                                </a>
+                                <p className="mt-2 text-gray-600">
+                                    {d.Description}
+                                </p>
+                            </div>
+                            <div className="flex justify-between items-center mt-4">
+                                <a className="text-blue-600 hover:underline" href={`http://localhost:4000/images/${d.File}`} download="assignment.pdf">
+                                    Download
+                                </a>
+                                <div>
+                                    <a className="flex items-center" href="/">
+                                        <img
+                                            className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+                                            src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
+                                            alt="avatar"
+                                        />
+                                        <h1 className="text-gray-700 font-bo    ld">{username}</h1>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             <Footer />
--- frontend/src/components/exam.jsx
+++ frontend/src/components/exam.jsx
@@ -1,22 +1,193 @@
 import Footer from "./layout/footer";
+import { useState, useEffect } from "react";
 import Navbar from "./layout/navbar";
+import axios from "axios";
+import {
+    MDBInput,
+} from "mdb-react-ui-kit";
+
+const Modal = ({ children }) => {
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                {children}
+            </div>
+        </div>
+    );
+};
 
 const Exam = () => {
+    const [showModal, setShowModal] = useState(true);
+    const [accessible, setAccessible] = useState(false);
+    const [inputs, setInputs] = useState([{ question: '', totalnumber: 0, obtainnumber: 9999, answer: '' }, { question: '', totalnumber: 0, obtainnumber: 9999, answer: '' }]);
+    const [data, setdata] = useState([]);
+    const handleAddInput = () => {
+        setInputs([...inputs, { question: '', totalnumber: 0, obtainnumber: 0, answer: '' }]);
+    };
+    const handleInputChange = (index, event) => {
+        const { name, value } = event.target;
+        const newInputs = [...inputs];
+        if (newInputs[index]) {
+            newInputs[index][name] = value;
+            setInputs(newInputs);
+        }
+    };
+
+    useEffect(() => {
+        // Disable access to the content initially
+        setAccessible(false);
+        fetchData()
+    }, []);
+
+    const fetchData = async () => {
+        const Category = "exam"
+        try {
+            const response = await axios.get(`http://localhost:4000/assignment/getbycategory?Category=${Category}`);
+            if (response.status !== 200) {
+                throw new Error("Request failed.");
+            }
+            setdata(response.data);
+            console.log(response.data)
+        } catch (error) {
+            const logLevel = 'error'; // Define the log level for errors
+            const errorMessage = error.message; // Get the error message
+            const where = "student exam"; // Get the error message
+            const data1 = {
+                level: logLevel,
+                message: errorMessage,
+                where: where,
+            }
+            axios.post('http://localhost:4000/logs/add', data1)
+                .then((response) => {
+                    console.log("done");
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+        }
+    };
+
+    const handleStart = () => {
+        setAccessible(true); // Enable access to the content
+        setShowModal(false); // Hide the modal
+    };
+
+    const [answers, setAnswers] = useState([]);
+
+    const handleAnswerChange = (e, index) => {
+        const { value } = e.target;
+        const newAnswers = [...answers];
+
+        // Update or create the answer object based on the index
+        newAnswers[index] = {
+            ...newAnswers[index],
+            answer: value,
+        };
+
+        setAnswers(newAnswers);
+    };
+
+    const handleSubmit = async () => {
+
+        const name = data[0].Name
+        const description = data[0].Description
+        const date = data[0].Date
+        const category = data[0].Category
+        const username = localStorage.getItem("username")
+
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('description', description);
+        formData.append('date', date);
+        formData.append('category', category);
+        formData.append('Marks', data[0].Marks);
+        formData.append('file', null);
+        formData.append('username', username);
+        formData.append('teacher', data[0].TeacherName);
+
+
+        inputs.forEach((input, index) => {
+            formData.append(`inputs[${index}][question]`, data[0].Questions[index].question);
+            formData.append(`inputs[${index}][totalnumber]`, data[0].Questions[index].number);
+            formData.append(`inputs[${index}][obtainnumber]`, input.obtainnumber);
+            formData.append(`inputs[${index}][answer]`, input.answer);
+        });
+
+        try {
+            const response = await axios.post('http://localhost:4000/stuassignment/add', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            if (response.data.message === "added") {
+                window.location.href = '/home'
+            } else if (response.data.message === "already") {
+                document.getElementById("desk-error").innerHTML = "Course ALREADY EXIST";
+                document.getElementById("desk-error").style.color = "red";
+                document.getElementById("desk-error").style.display = "block";
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
     return (
         <div className="exam_page">
             <Navbar />
-            <div className="content relative mt-16 mb-8 sm:mx-[12%]">   
-            <h1 className="text-3xl">Exams</h1>
-                <section class="radio-section">
-                    <div class="radio-list">
-                        <h1>Which Social Media you Often use?</h1>
-                        <div class="radio-item"><input name="radio" id="radio1" type="radio" /><label for="radio1">INSTAGRAM</label></div>
-                        <div class="radio-item"><input name="radio" id="radio2" type="radio" /><label for="radio2">TWITTER</label></div>
-                        <div class="radio-item"><input name="radio" id="radio3" type="radio" /><label for="radio3">LINKEDIN</label></div>
-                        <div class="radio-item"><input name="radio" id="radio4" type="radio" /><label for="radio4">FACEBOOK</label></div>
+            {showModal && (
+                <Modal>
+                    <div className="flex items-center justify-center h-screen">
+                        <button className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800" onClick={handleStart}>
+                            Start the exam
+                        </button>
                     </div>
-                </section>
-            </div>
+                </Modal>
+            )}
+            {!accessible && (
+                <div className="content relative mt-16 mb-8 sm:mx-[12%]">
+                    {/* Your content before accessing the exam */}
+                </div>
+            )}
+            {accessible && (
+                <div className="content relative mt-16 mb-8 sm:mx-[12%]">
+                    <h1 className="text-3xl">Exams</h1>
+                    <section className="flex justify-center">
+                        <div className="flex flex-col w-50">
+                            {data.length > 0 ? (
+                                data.map((item, iindex) => (
+                                    <div key={iindex} className="mb-4">
+                                        {item && item.Questions.map((item1, index) => {
+
+                                            return (
+                                                <div key={index}>
+                                                    <label htmlFor={`question-${index + 1}`}>
+                                                        {item1.question}
+                                                    </label>
+                                                    <textarea
+                                                        value={item1.answer}
+                                                        onChange={(event) => handleInputChange(index, event)}
+                                                        id={`question-${index + 1}`}
+                                                        className="border border-gray-300 rounded-md p-2 mt-2 block w-full"
+                                                        rows="4"
+                                                        name="answer"
+                                                        placeholder="Type your answer here..."
+                                                    ></textarea>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                ))
+                            ) : (
+                                <p>There are no exams</p>
+                            )}
+                            {data != "" ? (
+                                <button onClick={handleSubmit} className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800" >Submit Exam</button>
+                            ) : ""}
+                        </div>
+                    </section>
+                </div>
+            )}
             <Footer />
         </div>
     );
