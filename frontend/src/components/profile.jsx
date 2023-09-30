6684206c107ce45a361009337ba697eeb8a35fc0
Hamad-Younas <140589341+Hamad-Younas@users.noreply.github.com>
2023-09-30 19:11:09 +0500

    manage profile and reports student UI

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -1,58 +1,78 @@
+import Cookies from "js-cookie";
 import Footer from "./layout/footer";
 import Navbar from "./layout/navbar";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
+    const [data, setdata] = useState([])
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const handlelogout = () => {
+        Cookies.remove("token");
+        window.location.href = '/'
+    }
+
+
+    const fetchData = async () => {
+        const username = localStorage.getItem("username")
+        try {
+            const response = await axios.get(`http://localhost:4000/user/getbyusername?username=${username}`);
+            if (response.status !== 200) {
+                throw new Error("Request failed.");
+            }
+            setdata(response.data);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
     return (
         <div className="profile_page">
             <Navbar />
             <dir className="content">
                 <div class="flex items-center justify-center p-12">
                     <div class="mx-auto w-full max-w-[550px] bg-white">
-                        <form>
-                            <div class="mb-5">
-                                <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
-                                    Full Name
-                                </label>
-                                <input type="text" name="name" id="name" placeholder="Full Name"
-                                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                            </div>
-                            <div class="mb-5">
-                                <label for="phone" class="mb-3 block text-base font-medium text-[#07074D]">
-                                    Phone Number
-                                </label>
-                                <input type="text" name="phone" id="phone" placeholder="Enter your phone number"
-                                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                            </div>
-                            <div class="mb-5">
-                                <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
-                                    Email Address
-                                </label>
-                                <input type="email" name="email" id="email" placeholder="Enter your email"
-                                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                            </div>
-                
-
-                            <div class="mb-5 pt-3">
-                                <label class="mb-5 block text-base font-semibold text-[#07074D] sm:text-xl">
-                                    Address Details
-                                </label>
-                                <div class="-mx-3 flex flex-wrap">
-                                    <div class="w-full px-3">
-                                        <div class="mb-5">
-                                            <input type="text" name="area" id="area" placeholder="Enter area"
-                                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                        </div>
-                                    </div>
+                        {data.length > 0 && (
+                            <form>
+                                <div class="mb-5">
+                                    <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
+                                        First Name
+                                    </label>
+                                    <input type="text" name="name" id="name" value={data[0].Firstname}
+                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
                                 </div>
-                            </div>
+                                <div class="mb-5">
+                                    <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
+                                        Last Name
+                                    </label>
+                                    <input type="text" name="name" id="name" placeholder="Full Name" value={data[0].Lastname}
+                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                </div>
+                                <div class="mb-5">
+                                    <label for="phone" class="mb-3 block text-base font-medium text-[#07074D]">
+                                        Username
+                                    </label>
+                                    <input type="text" name="phone" id="phone" placeholder="Enter your phone number" value={data[0].Username}
+                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                </div>
+                                <div class="mb-5">
+                                    <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
+                                        Email Address
+                                    </label>
+                                    <input type="email" name="email" id="email" placeholder="Enter your email" value={data[0].email}
+                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                </div>
+                                <div>
+                                    <button
+                                        class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800" onClick={handlelogout}>
+                                       Logout
+                                    </button>
+                                </div>
 
-                            <div>
-                                <button
-                                    class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
-                                    Book Appointment
-                                </button>
-                            </div>
-                        </form>
+                            </form>
+                        )}
                     </div>
                 </div>
             </dir>
--- frontend/src/components/reports.jsx
+++ frontend/src/components/reports.jsx
@@ -1,35 +1,140 @@
+import { useEffect, useState } from "react";
 import Footer from "./layout/footer";
+import axios from "axios";
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import { saveAs } from 'file-saver';
 import Navbar from "./layout/navbar";
 
 const Reports = () => {
+    const [username, setusername] = useState("")
+    const [attendance, setattendance] = useState([])
+    const [marks, setmarks] = useState([])
+    useEffect(() => {
+        setusername(localStorage.getItem("username"))
+        fetchData()
+        fetchData1()
+    }, [])
+
+    const fetchData = async () => {
+        const Username = localStorage.getItem("username")
+        try {
+            const response = await axios.get(`http://localhost:4000/attendance/getuserattendance?Username=${Username}`);
+            if (response.status !== 200) {
+                throw new Error("Request failed.");
+            }
+            setattendance(response.data);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+    const fetchData1 = async () => {
+        const username = localStorage.getItem("username")
+        try {
+            const response = await axios.get(`http://localhost:4000/stuassignment/getbyusername?username=${username}`);
+            if (response.status !== 200) {
+                throw new Error("Request failed.");
+            }
+            setmarks(response.data);
+        } catch (error) {
+            const logLevel = 'error'; // Define the log level for errors
+            const errorMessage = error.message; // Get the error message
+            const where = "student reports"; // Get the error message
+            const data1 = {
+                level: logLevel,
+                message: errorMessage,
+                where: where,
+            }
+            axios.post('http://localhost:4000/logs/add', data1)
+                .then((response) => {
+                    console.log("done");
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+        }
+    };
+
+    const generateAndDownloadPDF = (data) => {
+        // Create a new jsPDF instance
+        const doc = new jsPDF();
+
+        // Format your data into a table or content for the PDF
+        // For example, using jspdf-autotable to create a table
+        const columns = Object.keys(data[0]);
+
+        // Extract values for each row
+        const rows = data.map(obj => Object.values(obj));
+
+        // Add the table with the extracted data to the PDF
+        doc.autoTable({
+            head: [columns],
+            body: rows,
+        });
+
+        // Save the PDF with a specific name (e.g., 'downloaded.pdf')
+        const pdfName = 'downloaded.pdf';
+
+        // Save the generated PDF
+        doc.save(pdfName);
+    };
     return (
         <div className="reports_page">
             <Navbar />
             <div className="content flex flex-col mt-16 mb-8 justify-center items-center">
                 <h1 className="text-3xl">Reports</h1>
-                <div className="max-w-4xl px-10 bg-white rounded-lg shadow-md">
-                    <div className="mt-2">
-                        <a className="text-2xl text-gray-700 font-bold hover:text-gray-600" href="/">
-                            Report
-                        </a>
-                        <p className="mt-2 text-gray-600">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque.
-                            Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!
-                        </p>
-                    </div>
-                    <div className="flex justify-between items-center mt-4">
-                        <a className="text-blue-600 hover:underline" href="/">
-                            Download
-                        </a>
-                        <div>
-                            <a className="flex items-center" href="/">
-                                <img
-                                    className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
-                                    src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
-                                    alt="avatar"
-                                />
-                                <h1 className="text-gray-700 font-bo    ld">Khatab wedaa</h1>
+                <div className="w-full max-w-4xl">
+                    <div className="max-w-4xl px-10 bg-white rounded-lg shadow-md">
+                        <div className="mt-2">
+                            <a className="text-2xl text-gray-700 font-bold hover:text-gray-600" href="/">
+                                Attendance Report
                             </a>
+                            <p className="mt-2 text-gray-600">
+                                Attendance report till this date
+                            </p>
+                        </div>
+                        <div className="flex justify-between items-center mt-4">
+                            <button className="text-blue-600 hover:underline" onClick={() => generateAndDownloadPDF(attendance)}>
+                                Download
+                            </button>
+                            <div>
+                                <a className="flex items-center" href="/">
+                                    <img
+                                        className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+                                        src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
+                                        alt="avatar"
+                                    />
+                                    <h1 className="text-gray-700 font-bo    ld">{username}</h1>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="my-6"></div>
+                <div className="w-full max-w-4xl">
+                    <div className="max-w-4xl px-10 bg-white rounded-lg shadow-md">
+                        <div className="mt-2">
+                            <a className="text-2xl text-gray-700 font-bold hover:text-gray-600" href="/">
+                                Marks Report
+                            </a>
+                            <p className="mt-2 text-gray-600">
+                                Marks report till this date
+                            </p>
+                        </div>
+                        <div className="flex justify-between items-center mt-4">
+                            <button className="text-blue-600 hover:underline" onClick={() => generateAndDownloadPDF(marks)}>
+                                Download
+                            </button>
+                            <div>
+                                <a className="flex items-center" href="/">
+                                    <img
+                                        className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+                                        src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80"
+                                        alt="avatar"
+                                    />
+                                    <h1 className="text-gray-700 font-bo    ld">{username}</h1>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,5 +143,5 @@ const Reports = () => {
         </div>
     );
 }
- 
+
 export default Reports;
